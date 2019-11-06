3d8ec5d1d4b70b6b006388d332dca03a3561ee81
Hong Zhang <h9124@msn.com>
2019-11-06 10:01:44 -0500

    Add project files.

---
+++ UnoApp1/UnoApp1.Shared/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace UnoApp1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        string _sDebug = "Click the image";
+        public string sDebug
+        {
+            get { return _sDebug; }
+            set
+            {
+                if (_sDebug != value)
+                {
+                    _sDebug = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        uint uiWidth = 30;
+        uint uiHeight = 30;
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
